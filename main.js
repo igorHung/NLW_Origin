4af944348e57dc771f13a040dd1181012e3aadc0
Igor hunguere <igor.tssa@hotmail.com>
2021-06-22 17:38:04 -0300

    2 dia - Nlw (maykao)
    Navigation com JS e icons

---
+++ main.js
@@ -0,0 +1,19 @@
+/* Abre e fecha quando clicar no icone do hamburguer e X */
+const nav = document.querySelector('header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Quando clicar em um item no menu, esconder o menu  */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
