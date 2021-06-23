8e7135ef9dfb919d9d56cf2b0eb1933a81ca8bd2
Igor hunguere <igor.tssa@hotmail.com>
2021-06-23 02:37:16 -0300

    Section Sobre e Serviços
    Section sobre e serviços e scroll em Js

--- nlw/main.js
+++ nlw/main.js
@@ -17,3 +17,16 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* Mudar o Header da pagina quando dar scroll  */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
